b0ccc4d9481e0e7924ae2e23fd1532953b5936a9
Samita Mondal <samitam@acquiscompliance.com>
2022-04-22 23:33:54 +0530

    Create initial template or design for the site

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
 import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
+
+import { Layout, Form, Input, Button, Card } from 'antd';
+const { Header, Footer, Sider, Content } = Layout;
+const { Meta } = Card;
 
 function App() {
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Layout className='layout'>
+        <Header className='header'>
+          <img src={logo} alt="qrcodereact" className='logo' />
+          QR Code Generator | React
+        </Header>
+        <Layout>
+          <Sider className='sidebar'>
+            <Form
+              name="basic"
+              layout="vertical"
+              onFinish={onFinish}
+              onFinishFailed={onFinishFailed}
+              autoComplete="off"
+            >
+              <Form.Item
+                label="Username"
+                name="username"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input your username!',
+                  },
+                ]}
+              >
+                <Input placeholder="Username" />
+              </Form.Item>
+
+              <Form.Item
+                label="Password"
+                name="password"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input your password!',
+                  },
+                ]}
+              >
+                <Input.Password placeholder="Password" />
+              </Form.Item>
+
+              <Form.Item
+              >
+                <Button type="primary" htmlType="submit">
+                  Submit
+                </Button>
+              </Form.Item>
+            </Form>
+          </Sider>
+          <Content className='content'>
+            <Card
+              hoverable
+              style={{ width: 300 }}
+              cover={<img alt="example" src="https://samita.in/assets/image/samita-qrcode.png" />}
+            >
+              <Meta title="Samita Mondal" description="https://samita.in/" />
+            </Card>
+          </Content>
+        </Layout>
+        <Footer className='footer'>
+          <p>&copy; Copyrights Samita M. All rights reserved.</p>
+        </Footer>
+      </Layout>
     </div>
   );
 }
